eb9aa8cd4fa7ecf85d72fb91b6efe98768f811ef
jschneiderm98 <julio_schneider@hotmail.com>
2021-04-01 11:33:15 -0300

    Top5 component

---
+++ src/components/Top5/index.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import './styles.css';
+
+export default function Top5() {
+  return (
+    <div className="Top5">
+      <div className="ComponentHeader">
+        Top5
+      </div>
+      <div className="ComponentBody">
+        <div className="Highest">
+          <span>Highest avg age</span>
+          <div className="ItemList">
+            <table>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="Lowest">
+          <span>Lowest avg age</span>
+          <div className="ItemList">
+            <table>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+              <tr>
+                <div className="Item">
+                  <td>Inter Milan</td>
+                  <td>31.9</td>
+                </div>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './styles.css';
-import MyTeams from '../../components/MyTeams';
+// import MyTeams from '../../components/MyTeams';
+import Top5 from '../../components/Top5';
 
 export default function Home() {
   return (
     <div className="Home">
-      <MyTeams />
+      <Top5 />
     </div>
   );
 }
